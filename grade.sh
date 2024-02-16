d529a295c8beb1b65fa28123c382f759e4d89b55
thomas-rocha <trocha@ucsd.edu>
2024-02-15 16:14:47 -0800

    Updates for Lab 6
    Lab 6 file updates to grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,43 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+if [[ -f "student-submission/ListExamples.java" ]]
+then
+    echo "File found!"
+else
+    echo "ListExamples.java not found!"
+    exit 1
+fi
+
+#move jar files, TestListExamples.java, and ListExamples.java to grading-area
+cp TestListExamples.java grading-area/
+cp -r lib grading-area/
+cp student-submission/ListExamples.java grading-area/
+
+cd grading-area
+
+javac -cp $CPATH *.java 2> trash-file.txt 
+
+if [[ $? -ne 0 ]]
+then 
+    echo "Compile error!"
+    exit 1;
+else
+    echo "Compiled successfully!"
+fi
+
+javac -cp $CPATH TestListExamples.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output-testing.txt
+
+if grep -q "Failures:" output-testing.txt; then
+    errCount=$(grep "Failures" output-testing.txt | awk '{print $5}')
+    totalCount=$(grep "run:" output-testing.txt | awk -F'[, ]' '{print $3}')
+    echo "Error count: $errCount"
+    successes=$((totalCount - errCount))
+    echo "Your grade is $successes / $totalCount"
+elif grep -1 "OK (" output-testing.txt; then 
+    echo "Total Grade: 100"
+else
+    echo "Error occurred while calculating test grades"
+fi
+
